6dad25e0e389c13a26b55d9dfa0ca1a2dac68391
IpshitaChatterjee <ipshita.chatterjee02@gmail.com>
2025-10-08 20:29:31 +0530

    fixed 404 error, added vercel analytics

--- next.config.mjs
+++ next.config.mjs
@@ -8,10 +8,21 @@ const nextConfig = {
     // Avoid failing the build on ESLint issues during deployment
     ignoreDuringBuilds: true,
   },
+  async redirects() {
+    return [
+      {
+        source:
+          '/blog/exploring-the-intersection-of-design-ai-and-design-engineering',
+        destination: '/blog/broadband-labels',
+        permanent: true,
+      },
+    ];
+  }
 };
 
 const withMDX = createMDX({
   extension: /\.mdx?$/,
 });
+
 
 export default withMDX(nextConfig);
