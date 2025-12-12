6ce2ee5b02aa2f1e33e6b128cc214ae3dd61bbb3
IpshitaChatterjee <ipshita.chatterjee02@gmail.com>
2025-12-12 15:12:14 +0530

    trying to fix bugs

--- app/data.ts
+++ app/data.ts
@@ -18,6 +18,13 @@ type WorkExperience = {
   description: string
 }
 
+type BlogPost = {
+  title: string
+  description: string
+  link: string
+  uid: string
+}
+
 type SocialLink = {
   label: string
   link: string
@@ -87,6 +94,41 @@ export const WORK_EXPERIENCE: WorkExperience[] = [
     link: 'https://www.infosys.com/',
     id: 'work4',
     description: 'iOS development for LEX, a cross-platform ed-tech application.',
+  },
+]
+
+export const BLOG_POSTS: BlogPost[] = [
+  {
+    title: 'Kitchen stories - simplifying meal planning',
+    description: 'Designing a lightweight system to make weekly cooking easier.',
+    link: 'https://www.behance.net/gallery/203789889/Kitchen-Stories-Redesign-gamified-cooking-experience',
+    uid: 'blog-5',
+  },
+  {
+    title: 'Exploring the Intersection of Design, AI, and Design Engineering',
+    description: 'How AI is changing the way we design',
+    link: '/blog/exploring-the-intersection-of-design-ai-and-design-engineering',
+    uid: 'blog-1',
+  },
+  {
+    title: 'Why I left my job to start my own company',
+    description:
+      'A deep dive into my decision to leave my job and start my own company',
+    link: '/blog/exploring-the-intersection-of-design-ai-and-design-engineering',
+    uid: 'blog-2',
+  },
+  {
+    title: 'What I learned from my first year of freelancing',
+    description:
+      'A look back at my first year of freelancing and what I learned',
+    link: '/blog/exploring-the-intersection-of-design-ai-and-design-engineering',
+    uid: 'blog-3',
+  },
+  {
+    title: 'How to Export Metadata from MDX for Next.js SEO',
+    description: 'A guide on exporting metadata from MDX files to leverage Next.js SEO features.',
+    link: '/blog/example-mdx-metadata',
+    uid: 'blog-4',
   },
 ]
 
